737115d35aa4e197a7a53bdcaf39bccacb738d2d
bahahr4 <baha.hermassi@esprit.tn>
2024-11-20 20:43:24 +0100

    baha hermasssii

--- src/Controller/ReclamationController.php
+++ src/Controller/ReclamationController.php
@@ -3,25 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Reclamation;
+use App\Entity\Reply;
 use App\Form\ReclamationType;
+use App\Form\ReplyType;
 use App\Repository\ReclamationRepository;
+use App\Repository\ReplyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/reclamation')]
 final class ReclamationController extends AbstractController
 {
+    // Route pour afficher la liste des réclamations
     #[Route(name: 'app_reclamation_index', methods: ['GET'])]
     public function index(ReclamationRepository $reclamationRepository): Response
     {
+        $reclamations = $reclamationRepository->findAll();
+
         return $this->render('reclamation/index.html.twig', [
-            'reclamations' => $reclamationRepository->findAll(),
+            'reclamations' => $reclamations,
         ]);
     }
 
+    // Route pour créer une nouvelle réclamation
     #[Route('/new', name: 'app_reclamation_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -42,6 +49,7 @@ final class ReclamationController extends AbstractController
         ]);
     }
 
+    // Route pour afficher une réclamation spécifique
     #[Route('/{id}', name: 'app_reclamation_show', methods: ['GET'])]
     public function show(Reclamation $reclamation): Response
     {
@@ -50,6 +58,7 @@ final class ReclamationController extends AbstractController
         ]);
     }
 
+    // Route pour modifier une réclamation existante
     #[Route('/{id}/edit', name: 'app_reclamation_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Reclamation $reclamation, EntityManagerInterface $entityManager): Response
     {
@@ -68,14 +77,38 @@ final class ReclamationController extends AbstractController
         ]);
     }
 
+    // Route pour supprimer une réclamation
     #[Route('/{id}', name: 'app_reclamation_delete', methods: ['POST'])]
     public function delete(Request $request, Reclamation $reclamation, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$reclamation->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $reclamation->getId(), $request->get('_token'))) {
             $entityManager->remove($reclamation);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('app_reclamation_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    // Route pour créer une nouvelle réponse à une réclamation spécifique
+    #[Route('/reply/new/{id}', name: 'app_reply_new', methods: ['GET', 'POST'])]
+    public function replyNew(Request $request, Reclamation $reclamation, EntityManagerInterface $entityManager): Response
+    {
+        $reply = new Reply();
+        $reply->setReclamation($reclamation); // Lier la réponse à la réclamation
+
+        $form = $this->createForm(ReplyType::class, $reply);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($reply);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_reclamation_show', ['id' => $reclamation->getId()]);
+        }
+
+        return $this->render('reclamation/replynew.html.twig', [
+            'reply' => $reply,
+            'form' => $form,
+        ]);
     }
 }
--- src/Entity/Reclamation.php
+++ src/Entity/Reclamation.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ReclamationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -17,11 +19,21 @@ class Reclamation
     #[ORM\Column(length: 255)]
     private ?string $type = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]  // Changement de 'string' à 'text' pour le contenu
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $datereclamation = null;
+
+    #[ORM\OneToMany(mappedBy: 'reclamation', targetEntity: Reply::class, cascade: ["persist", "remove"])]
+    private Collection $replies;
+
+    public function __construct()
+    {
+        $this->replies = new ArrayCollection();
+    }
+
+    // Getters and Setters
 
     public function getId(): ?int
     {
@@ -60,6 +72,36 @@ class Reclamation
     public function setDatereclamation(\DateTimeInterface $datereclamation): static
     {
         $this->datereclamation = $datereclamation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Reply>
+     */
+    public function getReplies(): Collection
+    {
+        return $this->replies;
+    }
+
+    public function addReply(Reply $reply): static
+    {
+        if (!$this->replies->contains($reply)) {
+            $this->replies->add($reply);
+            $reply->setReclamation($this); // Assurez-vous que la relation est correctement définie des deux côtés
+        }
+
+        return $this;
+    }
+
+    public function removeReply(Reply $reply): static
+    {
+        if ($this->replies->removeElement($reply)) {
+            // Set the owning side to null (unless already changed)
+            if ($reply->getReclamation() === $this) {
+                $reply->setReclamation(null);
+            }
+        }
 
         return $this;
     }
